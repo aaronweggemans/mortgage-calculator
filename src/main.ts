272e6f86f1ddc51fa17589d82b06d9458261fb19
aaronweggemans <aaronweggemans@hotmail.nl>
2026-01-08 23:40:16 +0100

    To Angular 21

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { AppComponent } from './app/app.component';
-import { importProvidersFrom } from '@angular/core';
+import { importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 
 bootstrapApplication(AppComponent, {
     providers: [
-        importProvidersFrom(BrowserModule, ReactiveFormsModule, MatStepperModule, MatSlideToggleModule, MatButtonModule, MatIconModule, MatButtonToggleModule),
+        provideZoneChangeDetection(),importProvidersFrom(BrowserModule, ReactiveFormsModule, MatStepperModule, MatSlideToggleModule, MatButtonModule, MatIconModule, MatButtonToggleModule),
     ]
 })
   .catch(err => console.error(err));
