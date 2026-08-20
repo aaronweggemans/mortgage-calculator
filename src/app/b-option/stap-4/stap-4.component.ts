448b1c2f38c5e5143f401460bbc0e887d648aea9
aaronweggemans <aaronweggemans@hotmail.nl>
2026-08-20 15:14:55 +0200

    Enhance styling and layout for step components and add new step 4

--- src/app/b-option/stap-4/stap-4.component.ts
+++ src/app/b-option/stap-4/stap-4.component.ts
@@ -1,10 +1,12 @@
-import { Component, output } from '@angular/core';
+import { Component, output, ViewEncapsulation } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-stap-4',
-  imports: [],
   templateUrl: './stap-4.component.html',
   styleUrl: './stap-4.component.scss',
+  encapsulation: ViewEncapsulation.None,
+  imports: [MatIcon],
 })
 export class Stap4Component {
   public readonly next = output<void>();
