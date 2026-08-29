808106ba1371037cd2c7786a51cd9bd1bc3f43af
aaronweggemans <aaronweggemans@hotmail.nl>
2026-08-29 09:01:15 +0200

    Refactor card and footer components for improved layout; enhance form handling in step 1

--- src/app/b-option/selected-step/footer/footer.component.ts
+++ src/app/b-option/selected-step/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, output } from '@angular/core';
+import { Component, ElementRef, output, viewChild } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
@@ -7,6 +7,8 @@ import { MatIcon } from '@angular/material/icon';
   templateUrl: './footer.component.html',
 })
 export class FooterComponent {
+  public readonly nextButton = viewChild.required<ElementRef<HTMLButtonElement>>('nextButton');
+
   public readonly next = output<void>();
   public readonly previous = output<void>();
 }
--- src/app/b-option/selected-step/stap-1/stap-1.component.ts
+++ src/app/b-option/selected-step/stap-1/stap-1.component.ts
@@ -1,13 +1,49 @@
-import { Component, output } from '@angular/core';
+import { Component, effect, output, signal, viewChild } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { HeaderComponent } from '../header/header.component';
 import { FooterComponent } from '../footer/footer.component';
+import { form, FormField, maxDate, required, SchemaPathTree } from '@angular/forms/signals';
 
 @Component({
   selector: 'app-stap-1',
-  imports: [MatIcon, HeaderComponent, FooterComponent],
+  imports: [MatIcon, HeaderComponent, FooterComponent, FormField],
   templateUrl: './stap-1.component.html',
 })
 export class Stap1Component {
+  private readonly footer = viewChild.required(FooterComponent);
+
   public readonly next = output<void>();
+
+  private readonly dateAndStatusModal = signal<DateAndStatus>({
+    dateOfBirth: null,
+    status: '',
+  });
+
+  protected readonly form = form<DateAndStatus>(
+    this.dateAndStatusModal,
+    () => (schemaPath: SchemaPathTree<DateAndStatus>) => {
+      required(schemaPath.dateOfBirth, { message: 'U moet hier een valide waarde invullen.' });
+      maxDate(schemaPath.dateOfBirth, new Date(), {
+        message: 'U kunt geen datum in de toekomst invullen.',
+      });
+      required(schemaPath.status, { message: 'U moet hier een valide waarde invullen.' });
+    },
+  );
+
+  private readonly enableAndDisableNextButton = effect(() => {
+    console.log(this.form().valid());
+    if (this.footer().nextButton()) {
+      this.footer().nextButton().nativeElement.disabled = this.form().valid();
+    }
+  });
+
+  protected submitForm(): void {
+    //  this.formError.emit(this.form().invalid());
+    // this.formData.emit(this.form().value());
+  }
+}
+
+interface DateAndStatus {
+  dateOfBirth: Date | null;
+  status: '';
 }
